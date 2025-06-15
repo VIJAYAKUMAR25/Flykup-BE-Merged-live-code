efb0f749e31013fcaf4bd1255069e87375ea5f5d
VIJAYAKUMAR25 <vijay31797@gmail.com>
2025-06-15 19:47:40 +0530

    changes db

--- src/config/connection.js
+++ src/config/connection.js
@@ -6,10 +6,10 @@ const db_cluster = process.env.MONGO_CLUSTER;
 const db_app = process.env.MONGO_APP_NAME;
 
 // Development DataBase
-// const db_name = process.env.MONGO_DEV_DB;
+const db_name = process.env.MONGO_DEV_DB;
 
 // Production DataBase
-const db_name = process.env.MONGO_PROD_DB;
+// const db_name = process.env.MONGO_PROD_DB;
 
 
 const cloudURL = `mongodb+srv://${db_username}:${db_password}@${db_cluster}/${db_name}?retryWrites=true&w=majority&appName=${db_app}`;

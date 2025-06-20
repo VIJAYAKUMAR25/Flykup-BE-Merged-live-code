f6dc7ea4094f35b4e941c810dea7492791733027
VIJAYAKUMAR25 <vijay31797@gmail.com>
2025-06-20 17:50:51 +0530

    changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -13,6 +13,7 @@ export const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
             origin: [ 
+                
                 "http://localhost:5173",
                 "http://localhost:5174",
                 "https://app.flykup.live",
